21c2104df4fba181c443c80892dbdd1cb3fa640f
Ahmed Nawaz Gondal <154045292+nawaz0721@users.noreply.github.com>
2025-03-04 08:58:29 +0500

    cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://co-curriculum-activities-cs-it.vercel.app"],
+    origin: ["https://co-curriculum-activities-cs-it.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
