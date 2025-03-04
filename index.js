37a97901f3778b4be5acf2b28a064d98cb956ef6
Ahmed Nawaz Gondal <154045292+nawaz0721@users.noreply.github.com>
2025-03-04 07:50:08 +0500

    google auth

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
 import cors from "cors";
-import 'dotenv/config'
-import mongoose from 'mongoose';
+import "dotenv/config";
+import mongoose from "mongoose";
 import authRoutes from "./routers/auth.js";
+import passport from "passport";
+import session from "express-session";
 
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 const app = express();
+
+app.use(helmet()); // Security Headers
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // 100 requests per IP
+  })
+);
+
 // const PORT = 4000;
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
-  
+  cors({
+    origin: ["http://localhost:5173", "https://co-curriculum-activities-cs-it.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('mongodb Connected'))
-.catch(err => console.log('mongodb Connection Error: ', err));
+app.use(
+  session({ secret: process.env.GOOGLE_CLIENT_SECRET , resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose
+  .connect(process.env.MONGODB_URI , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongodb Connected"))
+  .catch((err) => console.log("mongodb Connection Error: ", err));
 
 // Use routes
 app.use("/auth", authRoutes);
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(process.env.PORT , () => console.log('SERVER IS RUNNING'))
+app.listen(process.env.PORT, () => console.log("SERVER IS RUNNING"));
