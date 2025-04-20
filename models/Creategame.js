90832e5c500b78f6bff904385dfcf3bedff774a2
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2025-04-21 01:42:42 +0500

    game created

--- models/Creategame.js
+++ models/Creategame.js
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
-// import Category from "./Category";
-// import { required } from "joi";
 const { Schema } = mongoose;
 
 const creategameSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  //   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  category: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
   description: { type: String, required: true },
   gameImageUrl: { type: String, required: true },
+  gameImagePublicId: { type: String, required: true },
   date: { type: String, required: true },
   time: { type: String, required: true },
-  //   lead: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  //   coLead: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  lead: { type: String, required: true },
-  coLead: { type: String, required: true },
+  lead: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  coLead: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
   price: { type: Number, required: true },
   player: { type: Number, required: true },
   venue: { type: String, required: true },
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,10 +1,9 @@
-// backend/cloudinary.js
-const { v2 } = require("cloudinary");
+import cloudinary from "cloudinary";
 
-v2.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = v2;
+export default cloudinary.v2;
