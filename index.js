f9e8e26e3c3ade133ec45f1ba7773a8b75b4667e
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2025-06-18 06:48:42 +0500

    frontend URl Updated

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 // https://co-curriculum-activities-cs-it.vercel.app
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://co-curriculum-activities-cs-it.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
